77d3fd57998f00b1ad7fcc87d330ce80895e41d7
Raúl Yeguas <neokore@gmail.com>
2014-10-30 21:53:55 +0100

    Added User read API

---
+++ user.js
@@ -0,0 +1,27 @@
+var conf = module.parent.parent.require('./meta').config,
+    db = module.parent.parent.require('../src/database'),
+    coreUser = module.parent.parent.require('../src/user');
+
+User = {};
+
+User.init = function(app){
+
+    this.baseURL = conf["api:baseURL"] + "/users";
+
+    app.get(this.baseURL+"/:slug",function(req, res, next){
+        coreUser.getUidByUserslug(req.params.slug, function(err, uid){
+            if(!err){
+                coreUser.getUserData(uid, function(err, userdata){
+                    /*res.header("Access-Control-Allow-Origin", "*");
+                    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");*/
+                    res.status(200).json(userdata);
+                });            
+            }else{
+                res.status(404);
+            }
+        });     
+    });
+};
+
+
+module.exports = User
